786f625e69e21109be0f02090e93facd57503c0b
ejoi8 <fadzli.freelance@gmail.com>
2025-06-09 08:39:42 +0800

    Remove action buttons from payment failed page for a cleaner user experience

--- resources/views/pages/payment-failed.blade.php
+++ resources/views/pages/payment-failed.blade.php
@@ -7,16 +7,5 @@
         @if($payment)
             <livewire:payment-status :payment-model="$payment" :auto-refresh="false" />
         @endif
-        
-        <div class="mt-8 space-x-4">
-            <a href="{{ url()->previous() }}" 
-               class="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md transition duration-200">
-                Try Again
-            </a>
-            <a href="/" 
-               class="bg-gray-600 hover:bg-gray-700 text-white px-6 py-3 rounded-md transition duration-200">
-                Go Home
-            </a>
-        </div>
     </div>
 </x-payment-gateway::payment-layout>
